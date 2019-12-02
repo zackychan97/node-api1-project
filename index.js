507f199e04841033f84f4b5f7a7a3ee1fd3c99f9
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-02 14:16:42 -0600

    Add the dependency express, add the import of express to index.js as well as import the database file, create the server and the port in the index.js file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // implement your API here
+
+const express = require('express'); //import express
+const db = require('./data/db'); //import database file
+const server = express(); // create our server
+const port = 4000; // create our port
+
+server.use(express.json()); // needed to parse JSON from the body
